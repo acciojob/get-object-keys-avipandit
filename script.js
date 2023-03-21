b61c0d8483744e16fb50d8570f5a61708bb1982f
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-21 19:37:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student = {
+  name: "John Doe"
+};
+
+// Add the getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Call the getKeys() method on the student object
+const keys = student.getKeys();
+console.log(keys);
